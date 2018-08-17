3bd88aefba08f626acda6be6b95dd70b1276188e
ankur-sardar <ankur.sardar18@gmail.com>
2018-08-17 14:20:56 +0900

    add feature to create new branch on new issue

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
  * This is the entry point for your Probot App.
  * @param {import('probot').Application} app - Probot's Application class.
  */
+
+ // Create New Branch on issue number
+const createNewBranch = require('./lib/create-new-branch')
+
+
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+
+  // Add a branch on issue name
+  robot.on(`issues.opened`, createNewBranch)
 
   // For more information on building apps:
   // https://probot.github.io/docs/
